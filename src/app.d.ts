554073027cb9de18f497e97026d4eed145fa75fd
GoldFrite <61522145+GoldFrite@users.noreply.github.com>
2026-01-04 21:37:58 +0100

    Add Google Analytics integration and page tracking
    Integrated Google Analytics by adding gtag script to app.html and updating global types for dataLayer and gtag in app.d.ts. Implemented page view tracking on navigation in +layout.svelte, ensuring analytics only runs in production.

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,13 +1,19 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
 declare global {
-	namespace App {
-		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
-		// interface PageState {}
-		// interface Platform {}
-	}
+  namespace App {
+    // interface Error {}
+    // interface Locals {}
+    // interface PageData {}
+    // interface PageState {}
+    // interface Platform {}
+  }
+  interface Window {
+    dataLayer: any[]
+    gtag: (...args: any[]) => void
+  }
 }
 
-export {};
+export {}
+
