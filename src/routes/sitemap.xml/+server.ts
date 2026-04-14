3d0058695de2b42ce19a1b30c638ed0a2c76ea31
GoldFrite <61522145+GoldFrite@users.noreply.github.com>
2026-04-14 18:58:47 +0200

    Fix

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -7,21 +7,24 @@ export const GET: RequestHandler = async () => {
   const pages = ['', '/packages', '/packages/lib', '/packages/admintool']
   const docPages = docsMenu.flatMap((section) => section.items.map((item) => `/docs/${item.slug}`))
   const blogModules = import.meta.glob('/src/lib/blog/*.md', { eager: true })
-  const blogPages = Object.entries(blogModules).map(([, mod]: any) => `/blog/${mod.metadata?.slug}`)
+  const blogPages = Object.entries(blogModules)
+    .map(([, mod]: any) => mod.metadata?.slug)
+    .filter(Boolean)
+    .map((slug) => `/blog/${slug}`)
 
   const allPages = [...pages, ...docPages, ...blogPages]
 
-  const sitemap = `<?xml version="1.0" encoding="UTF-8" ?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${allPages
-    .map(
-      (page) => `
-<url>
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${allPages
+  .map(
+    (page) => `<url>
 <loc>${site}${page}</loc>
 <changefreq>weekly</changefreq>
 <priority>${page === '' ? 1.0 : 0.8}</priority>
 </url>`
-    )
-    .join('')}
+  )
+  .join('\n')}
 </urlset>`.trim()
 
   return new Response(sitemap, {
@@ -31,5 +34,3 @@ export const GET: RequestHandler = async () => {
     }
   })
 }
-
-
