93b1dffb2b7537b921db992be31b8a67b0ce56d0
GoldFrite <goldfrite@gmail.com>
2026-04-16 12:43:04 +0200

    Create Discord links

---
+++ src/routes/discord/reddit/+page.server.ts
@@ -0,0 +1,8 @@
+import { redirect } from '@sveltejs/kit'
+import type { PageServerLoad } from './$types'
+
+export const load = (async () => {
+  throw redirect(302, 'https://discord.gg/vepWkGY9Dk')
+}) satisfies PageServerLoad
+
+
